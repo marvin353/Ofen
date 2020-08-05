039b15f49c449faf30ab1be887ea645d0c8f722f
marvinAtPi <test@emal.com>
2020-08-05 12:14:26 +0200

    pi 1

--- OfenSelfControl/Gui.py
+++ OfenSelfControl/Gui.py
@@ -1,34 +1,39 @@
-#import webview
-
-#webview.create_window('Hello world', 'https://pywebview.flowrl.com/')
-#webview.create_window('OfenWatch', 'http://localhost/controlfrog/layouts/b/layout-8.html')
-#webview.start()
-
 import eel
+from datetime import datetime
 
 class Gui(object):
 
-    def __init__(self, ofen):
+    def __init__(self):
         print("Init Gui")
 
-        self.ofen = ofen
+        #self.ofen = ofen
 
+        now = datetime.now()
+        text_file = open("/home/pi/Desktop/OfenAlt/TEXT.txt", "w")
+        n = text_file.write(str(now))
+        text_file.close()
+        
+        my_options = {
+          'mode': "chromium", #or "chrome-app",
+          'host': 'localhost',
+          'port': 8000,
+          'chromeFlags': ["--start-fullscreen", "--browser-startup-dialog"]
+        }
+        
         eel.init('web')
-        #my_options = {
-        #    'mode': "chrome",  # or "chrome-app",
-        #    'host': 'localhost',
-        #    'port': 8080,
-        #    'chromeFlags': ["--start-fullscreen", "--browser-startup-dialog"]
-        #}
-        eel.start('controlfrog/layouts/b/layout-8.html')
+        eel.start('controlfrog/layouts/b/layout-8.html', options=my_options, suppress_error = True)
+
 
     # call the javascript function update the view
     def updateView(self):
         i = 0
 
-    def checkForAlert(self):
-        alerts = self.ofen.get_alertCodes
+   # def checkForAlert(self):
+        #alerts = self.ofen.get_alertCodes
 
-    def showFailureOverlay(self):
+    #def showFailureOverlay(self):
+		
 
+
+gui = Gui()
 
